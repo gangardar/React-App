103b6504f5d02230b8a4197b8ffb7a3233d5e4e3
gangardar <ggdgangardarggd@gmail.com>
2023-05-16 12:01:56 +0630

    Initial Commited

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+function Message() {
+  const name = "Mosh";
+  return (
+    <>
+      <h1> Hello {name} World!</h1>
+    </>
+  );
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function ListGroup() {
+  const items = ["NewYork", "San Francisco", "Tokyo", "London", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List </h1>
+      {items.length === 0 ? <p>No item found</p> : null}
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
